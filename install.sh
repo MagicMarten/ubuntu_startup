717f3d9fcdb57552268f12bbb6dc95fdbdedc63f
MartenMeijboom <marten1998@hotmail.com>
2020-10-29 03:20:21 +0100

    add discord and teams

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ apt-get update
 
 # Get DEB files
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.3.00.5153_amd64.deb
 
 # Install from Repo
 apt install git -y
@@ -29,12 +30,15 @@ snap install intellij-idea-community --classic
 snap install --classic code
 snap install spotify
 snap install postman
+snap install discord
 
 # Install DEB files
 dpkg -i google-chrome-stable_current_amd64.deb
+dpkg -i teams_1.3.00.5153_amd64.deb
 
 # Clean up DEB files
-
+rm -f google-chrome-stable_current_amd64.deb
+rm -f teams_1.3.00.5153_amd64.deb
 
 echo All application have been installed, the script will now quit. 
 exit 0
